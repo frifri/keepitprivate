817733d0432a200faa8ce5a204104363bd4179bb
Francis Genet <francis.genet@gmail.com>
2019-04-08 23:34:51 -0700

    Adding base url file

--- keepitprivate/urls.py
+++ keepitprivate/urls.py
@@ -1,8 +1,18 @@
 """
-keepitprivate URL Configuration
+keepitprivate base URL Configuration
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+
+from keepitprivate.constants import DEV
+
 
 urlpatterns = [
+    path('auth/', include('keepitprivate.api_service.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.ENVIRONMENT == DEV:
+    urlpatterns += [
+        path('api-auth/', include('rest_framework.urls')),
+    ]
